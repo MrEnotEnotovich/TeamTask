956c5ef3e42b9c64fa1ef2455b274954c07449ea
MrEnotEnotovich <kirill123456789094@gmail.com>
2022-12-14 21:53:37 +0300

    Added Header calculator.h

---
+++ calculator.h
@@ -0,0 +1,9 @@
+
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+double MySum(double, double);
+double MySub(double, double);
+double MyMul(double, double);
+double MyDiv(double, double);
+double MyPow(double, double);
+#endif // __CALCULATOR_H__
